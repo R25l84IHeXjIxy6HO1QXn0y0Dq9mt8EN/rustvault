a23138185ef391743cbcfcd11b55959064a99e38
R25l84IHeXjIxy6HO1QXn0y0Dq9mt8EN <67037013+R25l84IHeXjIxy6HO1QXn0y0Dq9mt8EN@users.noreply.github.com>
2020-06-23 10:27:37 -0700

    Add error types and URI parameter encoding

--- src/lib.rs
+++ src/lib.rs
@@ -3,17 +3,40 @@ mod util;
 
 use util::*;
 
+use std::error::Error;
 use std::fmt;
 
 use hyper::Body;
+use percent_encoding::{NON_ALPHANUMERIC, utf8_percent_encode};
+use serde::{Deserialize, Serialize};
 use serde_json::{json, Value};
 
-pub type JSONResult<T> = Result<T, Box<dyn std::error::Error>>;
+pub type BoxedResult<T> = Result<T, Box<dyn std::error::Error>>;
 
 #[derive(Debug)]
 pub struct Vault<'a> {
     name: &'a str,
     token: &'a str
+}
+
+#[derive(Debug, Deserialize)]
+pub struct VaultError<'a> {
+    #[serde(borrow)]
+    error: ErrorContainer<'a>
+}
+
+#[derive(Serialize)]
+pub struct Key<'a> {
+    id: &'a str,
+    value: &'a str
+}
+
+#[derive(Debug, Deserialize)]
+struct ErrorContainer<'a> {
+    code: &'a str,
+    message: &'a str,
+    #[serde(rename(deserialize = "innererror"))]
+    inner_error: Option<Box<ErrorContainer<'a>>>
 }
 
 impl fmt::Display for Vault<'_> {
@@ -22,8 +45,20 @@ impl fmt::Display for Vault<'_> {
     }
 }
 
+impl fmt::Display for VaultError<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "VaultError({}): {}", self.error.code, self.error.message)
+    }
+}
 
-pub async fn get_secrets(vault: Vault<'_>) -> JSONResult<Value> {
+impl Error for VaultError<'_> { }
+
+#[inline]
+fn encode_parameter(param: &str) -> String {
+    utf8_percent_encode(param, NON_ALPHANUMERIC).to_string()
+}
+
+pub async fn get_secrets(vault: Vault<'_>) -> BoxedResult<Value> {
     let resource_name = "/secrets";
 
     let req = vault_request_proto(vault, &resource_name)?
@@ -34,21 +69,10 @@ pub async fn get_secrets(vault: Vault<'_>) -> JSONResult<Value> {
         .await
 }
 
-pub async fn get_secret_versions(vault: Vault<'_>, secret_name: &str) -> JSONResult<Value> {
-    let resource_name = format!("/secrets/{}", secret_name);
-
-    let req = vault_request_proto(vault, &resource_name)?
-        .method("GET")
-        .body(Body::empty())?;
-
-    slurp_json(req)
-        .await
-}
-
-pub async fn get_secret(vault: Vault<'_>, secret_name: &str, secret_version: &str) -> JSONResult<Value> {
+pub async fn get_secret_versions(vault: Vault<'_>, secret_name: &str) -> BoxedResult<Value> {
     let resource_name = format!(
-        "/secrets/{}/version/{}",
-        secret_name, secret_version
+        "/secrets/{}",
+        encode_parameter(secret_name)
     );
 
     let req = vault_request_proto(vault, &resource_name)?
@@ -59,8 +83,26 @@ pub async fn get_secret(vault: Vault<'_>, secret_name: &str, secret_version: &st
         .await
 }
 
-pub async fn set_secret(vault: Vault<'_>, secret_name: &str, secret_value: &str) -> JSONResult<Value> {
-    let resource_name = format!("/secrets/{}", secret_name);
+pub async fn get_secret(vault: Vault<'_>, secret_name: &str, secret_version: Option<&str>) -> BoxedResult<Value> {
+    let resource_name = format!(
+        "/secrets/{}/version/{}",
+        encode_parameter(secret_name),
+        encode_parameter(secret_version.unwrap_or_default())
+    );
+
+    let req = vault_request_proto(vault, &resource_name)?
+        .method("GET")
+        .body(Body::empty())?;
+
+    slurp_json(req)
+        .await
+}
+
+pub async fn set_secret(vault: Vault<'_>, secret_name: &str, secret_value: &str) -> BoxedResult<Value> {
+    let resource_name = format!(
+        "/secrets/{}",
+        encode_parameter(secret_name)
+    );
     let payload = json!({
         "value": secret_value
     });
--- src/util.rs
+++ src/util.rs
@@ -1,12 +1,14 @@
-use crate::{JSONResult, Vault};
+use crate::{BoxedResult, Vault, VaultError};
 use crate::constants;
 
 use bytes::buf::BufExt;
 use hyper::{Body, Client, Request, Uri};
+use hyper::body::aggregate;
 use hyper::client::HttpConnector;
 use hyper::http::request::Builder;
 use hyper::http::uri::InvalidUri;
 use hyper_tls::HttpsConnector;
+use serde::Deserialize;
 use serde::de::DeserializeOwned;
 
 pub fn https_client() -> Client<HttpsConnector<HttpConnector>> {
@@ -22,12 +24,19 @@ pub fn vault_request_proto(v: Vault, resource_name: &str) ->  Result<Builder, In
         .header("Authorization", format!("Bearer {}", v.token)))
 }
 
-pub async fn slurp_json<T: DeserializeOwned>(req: Request<Body>) -> JSONResult<T>
-{
+pub async fn slurp_json<T: DeserializeOwned>(req: Request<Body>) -> BoxedResult<T> {
     let client = https_client();
     let res = client.request(req)
         .await?;
-    let body = hyper::body::aggregate(res)
-        .await?;
-    Ok(serde_json::from_reader(body.reader())?)
+
+    let status = res.status();
+    let body = aggregate(res).await?;
+    if status.is_success() {
+        Ok(serde_json::from_reader(body.reader())?)
+    }
+    else {
+        let mut de = serde_json::Deserializer::from_reader(body.reader());
+        let e = VaultError::deserialize(&mut de)?;
+        Err(Box::from(e))
+    }
 }
