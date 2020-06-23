40213ff67e0b8c6d59c043ed016c2a9881c44754
R25l84IHeXjIxy6HO1QXn0y0Dq9mt8EN <67037013+R25l84IHeXjIxy6HO1QXn0y0Dq9mt8EN@users.noreply.github.com>
2020-06-22 21:19:47 -0700

    Move all API calls to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,12 @@
-/*
-mod certificate;
-mod secret;
-mod key;
-mod storage;
-*/
-
 mod constants;
 mod util;
 
-pub mod secret;
+use util::*;
 
 use std::fmt;
+
+use hyper::Body;
+use serde_json::{json, Value};
 
 pub type JSONResult<T> = Result<T, Box<dyn std::error::Error>>;
 
@@ -24,4 +20,55 @@ impl fmt::Display for Vault<'_> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "https://{}.vault.azure.net", self.name)
     }
+}
+
+
+pub async fn get_secrets(vault: Vault<'_>) -> JSONResult<Value> {
+    let resource_name = "/secrets";
+
+    let req = vault_request_proto(vault, &resource_name)?
+        .method("GET")
+        .body(Body::empty())?;
+
+    slurp_json(req)
+        .await
+}
+
+pub async fn get_secret_versions(vault: Vault<'_>, secret_name: &str) -> JSONResult<Value> {
+    let resource_name = format!("/secrets/{}", secret_name);
+
+    let req = vault_request_proto(vault, &resource_name)?
+        .method("GET")
+        .body(Body::empty())?;
+
+    slurp_json(req)
+        .await
+}
+
+pub async fn get_secret(vault: Vault<'_>, secret_name: &str, secret_version: &str) -> JSONResult<Value> {
+    let resource_name = format!(
+        "/secrets/{}/version/{}",
+        secret_name, secret_version
+    );
+
+    let req = vault_request_proto(vault, &resource_name)?
+        .method("GET")
+        .body(Body::empty())?;
+
+    slurp_json(req)
+        .await
+}
+
+pub async fn set_secret(vault: Vault<'_>, secret_name: &str, secret_value: &str) -> JSONResult<Value> {
+    let resource_name = format!("/secrets/{}", secret_name);
+    let payload = json!({
+        "value": secret_value
+    });
+
+    let req = vault_request_proto(vault, &resource_name)?
+        .method("PUT")
+        .body(Body::from(serde_json::to_string(&payload)?))?;
+
+    slurp_json(req)
+        .await
 }
